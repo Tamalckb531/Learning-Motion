df2fc70bd74d6d3a42b4f09900d6b0812d517490
Tamal <ckbtamaldipnew@gmail.com>
2025-10-05 19:34:29 +0600

    Transition -> add Spinning Icon project

--- demos/src/app/page.tsx
+++ demos/src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <div className="">
       {/* <Box /> */}
-      <BouncingCircle />
-      {/* <SpinningIcon /> */}
+      {/* <BouncingCircle /> */}
+      <SpinningIcon />
       {/* <ComplexAnimation /> */}
       {/* <SkewRectangle /> */}
     </div>
--- demos/src/projects/Transition/SpinningIcon.tsx
+++ demos/src/projects/Transition/SpinningIcon.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import * as motion from "motion/react-client";
 
 const SpinningIcon = () => {
-  return <div>SpinningIcon</div>;
+  return (
+    <motion.img
+      src={"https://avatars.githubusercontent.com/u/134912393?v=4"}
+      className=" rounded-full"
+      animate={{ rotate: 360 }}
+      transition={{ duration: 2, repeat: Infinity, ease: "linear" }}
+    />
+  );
 };
 
 export default SpinningIcon;
