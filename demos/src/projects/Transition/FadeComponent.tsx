2c207e3864f1f95ec16a6b4cfa5f61e5f0270c0d
Tamal <ckbtamaldipnew@gmail.com>
2025-10-05 21:55:52 +0600

    add fade component

--- demos/src/projects/Transition/FadeComponent.tsx
+++ demos/src/projects/Transition/FadeComponent.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { motion } from "motion/react";
 
 const FadeComponent = () => {
-  return <div>FadeComponent</div>;
+  const [visible, setVisible] = useState(false);
+  return (
+    <div className=" flex flex-col gap-2 items-center justify-center">
+      <button
+        className=" mb-4 p-2 bg-blue-500 text-white rounded"
+        onClick={() => setVisible(!visible)}
+      >
+        Toggle Fade
+      </button>
+
+      {visible && (
+        <motion.div
+          className=" p-5 bg-gray-200"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          Hello, I'm fading component{" "}
+        </motion.div>
+      )}
+    </div>
+  );
 };
 
 export default FadeComponent;
