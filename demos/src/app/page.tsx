470bc43048fec7c335fae5493463a6db02c814c8
Tamal <ckbtamaldipnew@gmail.com>
2025-10-05 19:44:11 +0600

    Changed the project name to Transformation

--- demos/src/app/page.tsx
+++ demos/src/app/page.tsx
@@ -1,8 +1,8 @@
-import BouncingCircle from "@/projects/Transition/BouncingCircle";
-import Box from "@/projects/Transition/Box";
-import ComplexAnimation from "@/projects/Transition/ComplexAnimation";
-import SkewRectangle from "@/projects/Transition/SkewRectangle";
-import SpinningIcon from "@/projects/Transition/SpinningIcon";
+import BouncingCircle from "@/projects/Transformation/BouncingCircle";
+import Box from "@/projects/Transformation/Box";
+import ComplexAnimation from "@/projects/Transformation/ComplexAnimation";
+import SkewRectangle from "@/projects/Transformation/SkewRectangle";
+import SpinningIcon from "@/projects/Transformation/SpinningIcon";
 
 export default function Home() {
   return (
