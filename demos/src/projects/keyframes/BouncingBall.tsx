e8c869d4f676231e9fb4c228aed692391efcec6a
Tamal <ckbtamaldipnew@gmail.com>
2025-10-06 00:11:42 +0600

    Keyframe -> add bouncing ball

--- demos/src/projects/keyframes/BouncingBall.tsx
+++ demos/src/projects/keyframes/BouncingBall.tsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import * as motion from "motion/react-client";
 const BouncingBall = () => {
-  return <div>BouncingBall</div>;
+  return (
+    <motion.div
+      className=" w-16 h-16 bg-yellow-400 rounded-full"
+      animate={{
+        y: [100, -250, 100],
+      }}
+      transition={{
+        duration: 1,
+        repeat: Infinity,
+        repeatType: "loop",
+        ease: "easeInOut",
+      }}
+    />
+  );
 };
 
 export default BouncingBall;
