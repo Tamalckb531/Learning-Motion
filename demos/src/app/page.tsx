05907612e0be5e49785f8776ebb049cdcf56a504
Tamal <ckbtamaldipnew@gmail.com>
2025-10-05 19:26:20 +0600

    Transition -> add moving box project

--- demos/src/app/page.tsx
+++ demos/src/app/page.tsx
@@ -1,9 +1,17 @@
 import BouncingCircle from "@/projects/Transition/BouncingCircle";
+import Box from "@/projects/Transition/Box";
+import ComplexAnimation from "@/projects/Transition/ComplexAnimation";
+import SkewRectangle from "@/projects/Transition/SkewRectangle";
+import SpinningIcon from "@/projects/Transition/SpinningIcon";
 
 export default function Home() {
   return (
     <div className="">
-      <BouncingCircle />
+      <Box />
+      {/* <BouncingCircle /> */}
+      {/* <SpinningIcon /> */}
+      {/* <ComplexAnimation /> */}
+      {/* <SkewRectangle /> */}
     </div>
   );
 }
--- demos/src/projects/Transition/Box.tsx
+++ demos/src/projects/Transition/Box.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import * as motion from "motion/react-client";
 
 const Box = () => {
-  return <div>Box</div>;
+  return (
+    <motion.div
+      className=" bg-blue-500 w-20 h-20"
+      initial={{ x: -100 }}
+      animate={{ x: 100 }}
+      transition={{ duration: 2 }}
+    />
+  );
 };
 
 export default Box;
