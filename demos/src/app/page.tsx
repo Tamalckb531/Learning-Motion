2371fe92c6018041a3b92fafb3225c4f8e836ee7
Tamal <ckbtamaldipnew@gmail.com>
2025-10-07 22:15:42 +0600

    Gestures -> add Gesture Based Image gallery

--- demos/src/app/page.tsx
+++ demos/src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       {/* <DraggableBox /> */}
       {/* <RotateOnDrag /> */}
       {/* <TapToChangeColor /> */}
-      <LongPressToChange />
-      {/* <GestureBasedImgGal /> */}
+      {/* <LongPressToChange /> */}
+      <GestureBasedImgGal />
     </div>
   );
 }
--- demos/src/projects/Gestures/GestureBasedImgGal.tsx
+++ demos/src/projects/Gestures/GestureBasedImgGal.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
+
+const images = [
+  "https://images.unsplash.com/photo-1501785888041-af3ef285b470",
+  "https://images.unsplash.com/photo-1499346030926-9a72daac6c63",
+  "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
+];
 
 const GestureBasedImgGal = () => {
-  return <div>GestureBasedImgGal</div>;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleSwipe = (_: any, info: any) => {
+    if (info.offset.y > 100)
+      setCurrentIndex((prev) => (prev - 1 + images.length) % images.length);
+    else if (info.offset.y < -100) {
+      setCurrentIndex((prev) => (prev + 1) % images.length);
+    }
+  };
+
+  return (
+    <div className=" relative w-72 h-72 overflow-hidden">
+      <AnimatePresence>
+        <motion.img
+          className=" w-full h-full rounded"
+          key={currentIndex}
+          src={images[currentIndex]}
+          alt="Yo this is an image"
+          drag="y"
+          dragConstraints={{ top: 0, bottom: 0 }}
+          onDragEnd={handleSwipe}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        />
+      </AnimatePresence>
+    </div>
+  );
 };
 
 export default GestureBasedImgGal;
