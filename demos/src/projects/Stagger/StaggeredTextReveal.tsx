8db8de639b569c641f95679f524ea5e93510e5a9
Tamal <ckbtamaldipnew@gmail.com>
2025-10-08 00:54:31 +0600

    Staggered -> add Staggered Text Reveal

--- demos/src/projects/Stagger/StaggeredTextReveal.tsx
+++ demos/src/projects/Stagger/StaggeredTextReveal.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import * as motion from "motion/react-client";
+
+const text = "Tamal Gonna Make it --- :)";
+
+const staggeredVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
 
 const StaggeredTextReveal = () => {
-  return <div>StaggeredTextReveal</div>;
+  return (
+    <motion.h1
+      className=" text-4xl font-bold text-white"
+      initial="hidden"
+      animate="visible"
+      variants={{ visible: { transition: { staggerChildren: 0.15 } } }}
+    >
+      {text.split("").map((char, index) => (
+        <motion.span key={index} variants={staggeredVariants}>
+          {char}
+        </motion.span>
+      ))}
+    </motion.h1>
+  );
 };
 
 export default StaggeredTextReveal;
