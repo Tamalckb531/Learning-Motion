66ec21f9b63a2f9c0ce88956428f5429784e4a7b
Tamal <ckbtamaldipnew@gmail.com>
2025-10-08 00:25:40 +0600

    Staggered -> add Staggered List

--- demos/src/projects/Stagger/StaggeredList.tsx
+++ demos/src/projects/Stagger/StaggeredList.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import * as motion from "motion/react-client";
+
+const items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+
+const staggerVar = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
 
 const StaggeredList = () => {
-  return <div>StaggeredList</div>;
+  return (
+    <motion.ul
+      initial="hidden"
+      animate="visible"
+      variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
+    >
+      {items.map((item, index) => (
+        <motion.li
+          variants={staggerVar}
+          key={index}
+          className=" mb-2 p-5 bg-yellow-300 rounded-lg"
+        >
+          {item}
+        </motion.li>
+      ))}
+    </motion.ul>
+  );
 };
 
 export default StaggeredList;
