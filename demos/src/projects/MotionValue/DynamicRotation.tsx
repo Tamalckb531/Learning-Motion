9e1dbcfd8ed08256fa880fb71bbca951c5a1301d
Tamal <ckbtamaldipnew@gmail.com>
2025-10-08 01:27:19 +0600

    MotionValue -> add Dynamic Rotation

--- demos/src/projects/MotionValue/DynamicRotation.tsx
+++ demos/src/projects/MotionValue/DynamicRotation.tsx
@@ -1,7 +1,23 @@
+"use client";
+import { motion, useMotionValue } from "motion/react";
 import React from "react";
 
 const DynamicRotation = () => {
-  return <div>DynamicRotation</div>;
+  const rotation = useMotionValue(0);
+
+  const rotateBox = () => {
+    rotation.set(rotation.get() + 45);
+  };
+
+  return (
+    <motion.div
+      style={{ rotate: rotation }}
+      onClick={rotateBox}
+      className=" w-32 h-32 bg-blue-500 rounded"
+    >
+      DynamicRotation
+    </motion.div>
+  );
 };
 
 export default DynamicRotation;
