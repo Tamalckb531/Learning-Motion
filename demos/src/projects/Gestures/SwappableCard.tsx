b9be600599f4659c9542a09c7cfd2e251a00cf1a
Tamal <ckbtamaldipnew@gmail.com>
2025-10-07 21:45:53 +0600

    Gestures -> add SwippableCard

--- demos/src/projects/Gestures/SwappableCard.tsx
+++ demos/src/projects/Gestures/SwappableCard.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import { motion } from "motion/react";
 
 const SwappableCard = () => {
-  return <div>SwappableCard</div>;
+  const [isRemoved, setIsRemoved] = useState(false);
+
+  const handleSwipe = (_: any, info: any) => {
+    if (info.offset.x > 100) setIsRemoved(true);
+    else if (info.offset.x < -100) setIsRemoved(true);
+  };
+
+  return (
+    <motion.div
+      className={` w-64 h-32 bg-blue-500 rounded-lg shadow-lg flex items-center  justify-center text-white ${
+        isRemoved ? "hidden" : ""
+      }`}
+      drag="x"
+      dragConstraints={{ left: -100, right: 100 }}
+      onDragEnd={handleSwipe}
+    >
+      Swipe Me!
+    </motion.div>
+  );
 };
 
 export default SwappableCard;
